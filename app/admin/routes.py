25bafd04e05ebfa70d9b6f06f7ad24020971a760
ydb5755 <yisroel.d.baum@gmail.com>
2024-09-17 11:06:51 +0300

    add admin page for viewing models

--- app/admin/routes.py
+++ app/admin/routes.py
@@ -1,6 +1,8 @@
 from app import db
 from app.admin import admin
 from app.users.models import User
+from app.campaigns.models import Campaign
+from app.main.models import Donation
 from app.users.forms import LoginForm, RegisterUserForm#, RequestResetForm, ResetPasswordForm, EditUserForm, AddUserForm
 from flask import render_template, redirect, url_for, flash, request
 from flask_login import login_required, login_user, current_user, logout_user
@@ -10,4 +12,8 @@ import os
 
 @admin.route('administration')
 def administration():
-    return render_template('administration.html')
+    users = User.query.all()
+    campaigns = Campaign.query.all()
+    return render_template('administration.html',
+                           users=users,
+                           campaigns=campaigns)
