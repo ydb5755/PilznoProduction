0b17e390741b8c572fab7bb41f1009b3d47e70a6
ydb5755 <yisroel.d.baum@gmail.com>
2024-09-18 18:48:26 +0300

    add campaign detail page route

--- app/campaigns/routes.py
+++ app/campaigns/routes.py
@@ -1,6 +1,7 @@
 from app import db
 from app.campaigns import campaigns
 from app.users.models import User
+from app.campaigns.models import Campaign
 # from forms import LoginForm, RequestResetForm, ResetPasswordForm, EditUserForm, AddUserForm
 from flask import render_template, redirect, url_for, flash, request
 from flask_login import login_required, login_user, current_user, logout_user
@@ -11,3 +12,9 @@ import os
 @campaigns.route('add_campaign')
 def add_campaign():
     return render_template('add_campaign.html')
+
+@campaigns.route('campaign_page/<campaign_id>')
+def campaign_page(campaign_id):
+    campaign = Campaign.query.filter_by(id=campaign_id).first()
+    return render_template('campaign_page.html',
+                           campaign=campaign)
