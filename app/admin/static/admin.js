fc90674ccc37edc3927f838176dfc261f1f53e38
ydb5755 <yisroel.d.baum@gmail.com>
2024-09-22 15:52:05 +0300

    change activity buttons added event listeners

--- app/admin/static/admin.js
+++ app/admin/static/admin.js
@@ -1,11 +1,14 @@
-var archiveButtons = document.getElementsByClassName('archive-button');
+const archiveButtons = document.getElementsByClassName('archive-button');
+const activityButtons = document.getElementsByClassName('change-activity-btn');
+
+for(let i = 0; i < activityButtons.length; i++){
+    activityButtons[i].addEventListener('change', e => {
+        console.log(e.target.checked, e.target.id)
+    })
+}
+
 for(let i = 0; i < archiveButtons.length; i++){
     archiveButtons[i].addEventListener('click', e => {
         console.log(e.target.id)
     })
 }
-
-
-// archiveButton.addEventListener('change', (e) => {
-//     console.log(e)
-// })
